aeb9d1eaa2ea15c9f9475f512ef48a4983cc172d
Grace Fang <115444762+fc87290118@users.noreply.github.com>
2026-06-24 14:01:05 +0800

    1.rename_samples

---
+++ rename_samples.sh
@@ -0,0 +1,48 @@
+cat > rename_samples.sh << 'EOF'
+#!/bin/bash
+
+mv 1-LJD34888_L3_1.fq.gz  15h_rep1_R1.fq.gz
+mv 1-LJD34888_L3_2.fq.gz  15h_rep1_R2.fq.gz
+
+mv 2-LJD34889_L3_1.fq.gz  15h_rep2_R1.fq.gz
+mv 2-LJD34889_L3_2.fq.gz  15h_rep2_R2.fq.gz
+
+mv 3-LJD34890_L3_1.fq.gz  15h_rep3_R1.fq.gz
+mv 3-LJD34890_L3_2.fq.gz  15h_rep3_R2.fq.gz
+
+mv 4-LJD34891_L3_1.fq.gz  24h_rep1_R1.fq.gz
+mv 4-LJD34891_L3_2.fq.gz  24h_rep1_R2.fq.gz
+
+mv 5-LJD34892_L3_1.fq.gz  24h_rep2_R1.fq.gz
+mv 5-LJD34892_L3_2.fq.gz  24h_rep2_R2.fq.gz
+
+mv 6-LJD34893_L3_1.fq.gz  24h_rep3_R1.fq.gz
+mv 6-LJD34893_L3_2.fq.gz  24h_rep3_R2.fq.gz
+
+mv 7-LJD34894_L3_1.fq.gz  48h_rep1_R1.fq.gz
+mv 7-LJD34894_L3_2.fq.gz  48h_rep1_R2.fq.gz
+
+mv 8-LJD34895_L3_1.fq.gz  48h_rep2_R1.fq.gz
+mv 8-LJD34895_L3_2.fq.gz  48h_rep2_R2.fq.gz
+
+mv 9-LJD34896_L3_1.fq.gz  48h_rep3_R1.fq.gz
+mv 9-LJD34896_L3_2.fq.gz  48h_rep3_R2.fq.gz
+
+mv 10-LJD34897_L3_1.fq.gz 72h_rep1_R1.fq.gz
+mv 10-LJD34897_L3_2.fq.gz 72h_rep1_R2.fq.gz
+
+mv 11-LJD34898_L3_1.fq.gz 72h_rep2_R1.fq.gz
+mv 11-LJD34898_L3_2.fq.gz 72h_rep2_R2.fq.gz
+
+mv 12-LJD34899_L3_1.fq.gz 72h_rep3_R1.fq.gz
+mv 12-LJD34899_L3_2.fq.gz 72h_rep3_R2.fq.gz
+
+mv 13-LJD34900_L3_1.fq.gz CA72h_rep1_R1.fq.gz
+mv 13-LJD34900_L3_2.fq.gz CA72h_rep1_R2.fq.gz
+
+mv 14-LJD34901_L3_1.fq.gz CA72h_rep2_R1.fq.gz
+mv 14-LJD34901_L3_2.fq.gz CA72h_rep2_R2.fq.gz
+
+mv 15-LJD34902_L3_1.fq.gz CA72h_rep3_R1.fq.gz
+mv 15-LJD34902_L3_2.fq.gz CA72h_rep3_R2.fq.gz
+EOF
